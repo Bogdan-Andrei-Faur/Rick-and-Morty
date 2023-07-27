0063068f2bad83869faf13d10a7c8bb7809cc19e
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-07-27 19:52:13 +0200

    Integrado ORM 1

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,23 @@
-//Dotenv
-require("dotenv").config();
-const {PORT} = process.env;
-//Server
+require("dotenv").config(); // Agrega al objeto "process" en la prop "env" nuestras variables
+const { PORT, DB_USER } = process.env;
+const { connectionTuki } = require("./DB_connection");
+
 const server = require("./app");
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
-});
+// Servidor ---> Base de datos
+// server.listen(PORT, async () => {
+//   await connectionTuki.sync({ force: true }); // EN CADA SYNC ELIMINA LAS TABLAS Y SUS REGISTROS Y LAS VUELVE A CREAR
+//   // connectionTuki.sync({ alter: true }); // EN CADA SYNC ALTERA LAS TABLAS Y HACE SUS CAMBIOS (si por ej, agregamos un atributo)
+//   console.log("DB SYNC");
+//   console.log("Server raised in port: " + PORT);
+// });
+
+// Base de datos ---> Servidor
+connectionTuki
+  .sync({ alter: true })
+  .then((value) => {
+    server.listen(PORT, () => {
+      console.log("Server & DDBB Running ✅");
+    });
+  })
+  .catch((err) => console.error(err));
